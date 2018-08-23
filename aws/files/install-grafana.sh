e5f1caae87e1271bd043d120f240877b062decd2
omarvides <omarvides@gmail.com>
2018-08-22 21:25:40 -0600

    Adding required packages

---
+++ aws/files/install-grafana.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Enabling epel-release repositories
+yum install -y epel-release
+
+# Installing and enabling firewall
+yum install firewalld
+systemctl enable firewalld
+
+# Adding ssh, http and https firewall rules
+firewall-cmd --permanent --zone=public --add-service=http 
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --permanent --zone=public --add-service=ssh
+firewall-cmd --reload
+
+# Listing firewall rules
+firewall-cmd --list-all
+
+# Installing nginx
+yum install -y nginx
+
+# TODO: Configuring nginx
+
+# Installing and enabling Grafana
+yum install -y https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.1.4-1.x86_64.rpm
+service grafana-server start
